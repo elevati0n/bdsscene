11b76797559cb1de5380c3023ed22a62dc4698d7
Sean Flaherty <seanadamflaherty@gmail.com>
2014-08-10 09:10:44 +0000

    alpha

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,38 @@
+class AppointmentsController < ApplicationController
+
+   def create 
+ 	@appointments = Appointment.create(appointment_params)
+ 	@user = User.find(appointment_params[:user_id])    
+   	if @appointments.save
+    		flash[:success] = "appointment created!"
+    		redirect_to @user
+   		end 
+   	else if @appointments.nil?
+     		redirect_to signin_path
+     		flash[:failure] = "Session Lost / Please Log in"
+	end
+#    if @appointments.save
+#      flash[:success] = "appointment created!"
+#      redirect_to :action => :show
+
+end
+  end
+ 
+ def index
+    @appointment = Appointments.all
+  end
+
+
+  def destroy
+  end
+
+  def show 
+	@appointment = Appointments.all
+  end 
+
+  private
+
+    def appointment_params
+      params.require(:appointment).permit(:startTime, :endTime, :day, :month, :year, :status, :user_id)
+    end
+
